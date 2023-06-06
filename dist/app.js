c7529ba096f6ca7fc374c3beb042a645f38a170a
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2023-06-06 10:26:24 +0600

    learned how to handle primitive type variables and learned about union types and how infered types in typescript

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,24 @@
 "use strict";
+//..........Project setup
 var message = "hello typeScript";
 var message2 = "Tata javaScript";
-console.log(message, message2, language);
+// console.log(message, message2, language)
+//....Primitive Types(String, Number and boolean). 
+var myName;
+myName = "Debabrata Saha";
+myName = "Shuvo";
+// myName = 45;
+// myName = false
+var age;
+age = 34;
+// age = 'twenty'
+var hasGf;
+hasGf = false;
+// hasGf = 0;
+//.... UnionTypes and how inferred types in typeScript. 
+var myAge = 56; //(allows both number , string, boolean and array type)
+myAge = 'fifty six';
+myAge = false;
+myAge = [];
+var hisName = "ElonVai";
+// hisName = "shuvo" (gives an error)
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,39 @@
+//..........Project setup
 const message = "hello typeScript"
 const message2 = "Tata javaScript"
-console.log(message, message2, language)
+// console.log(message, message2, language)
+
+//....Primitive Types(String, Number and boolean). 
+
+
+let myName : string;
+myName = "Debabrata Saha"
+
+myName = "Shuvo";
+
+// myName = 45;
+// myName = false
+
+let age : number;
+age = 34; 
+// age = 'twenty'
+
+let hasGf : boolean; 
+hasGf = false; 
+// hasGf = 0;
+
+
+//.... UnionTypes and how inferred types in typeScript. 
+
+let myAge: number | string | boolean | [] = 56; //(allows both number , string, boolean and array type)
+
+myAge = 'fifty six'
+myAge = false
+myAge = []
+
+
+let hisName : "ElonVai" | "ElonMusk" = "ElonVai"; 
+// hisName = "shuvo" (gives an error)
+
+
+
